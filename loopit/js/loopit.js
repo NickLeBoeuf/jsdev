e3ef3d826c53e7f6be87dddb8fc0212636fc165a
nickleboeuf <nick.leboeuf@gmail.com>
2015-04-17 00:17:29 +0200

    First modif, to display the logo. Changed name of the Canvas.

--- loopit/js/loopit.js
+++ loopit/js/loopit.js
@@ -1,3 +1,21 @@
-alert(" Start loopit.js");
+//alert(" Start loopit.js");
 
+var logoloopit;
+
+var initialisation = function() {
+ canvasLoopIt = document.getElementById('canvasLoopIt');
+ canvasLoopIt2DContext = canvasLoopIt.getContext('2d');
+ logoloopit = new Image();
+ logoloopit.src = "./img/Loopit.jpg";
+ canvasLoopIt2DContext.drawImage(logoloopit,100,0);
+
+}
+
+
+window.addEventListener('load', function() {
+ initialisation();
+ //setInterval(scrollingHorizontal, 50); 
+ //requestAnimId = window.requestAnimationFrame(scrollingHorizontal);
+
+});
  
