f6004eaf5ffe4bb729629c9346278170dcf47fe8
nickleboeuf <nick.leboeuf@gmail.com>
2015-04-19 22:52:27 +0200

    Ridge object properly defined ! Cells are linked together
    Correctly defined the Ridge object with getter and setters for
    the up,down,left,right properties. These properties are linked to the
    global common ridgeh/v arrays, making possible to have 2 cells pointing to the
    same ridge. And so modifying a ridge of one cell automatically modify the ridge
    of the adjacent cell.

--- loopit/js/loopit.js
+++ loopit/js/loopit.js
@@ -1,11 +1,32 @@
 //alert(" Start loopit.js");
 
+// Object Ridge creation
+function Ridge(row,col) {
+  this.row = row; 
+  this.col = col;
+  var up = null;
+  // Define Getters and setters of the ridge, filling the RidgeH/V commmon Arrays.
+  Object.defineProperty(this, "up", {
+    get: function() {return ridgeh[row][col];},
+    set: function(val) {ridgeh[row][col]=val;  }   });
+  Object.defineProperty(this, "down", {
+    get: function() {return ridgeh[row+1][col];},
+    set: function(val) {ridgeh[row+1][col]=val;  }   });
+  Object.defineProperty(this, "left", {
+    get: function() {return ridgev[row][col];},
+    set: function(val) {ridgev[row][col]=val;  }   });
+  Object.defineProperty(this, "right", {
+    get: function() {return ridgev[row][col+1];},
+    set: function(val) {ridgev[row][col+1]=val;  }   });
+}
+ 
+ 
 //  Object "Cell" creation
 function Cell(row,col) {
   this.row=row;
   this.col=col;
   this.number = 0;
-  this.ridge = new Ridge;
+  this.ridge = null;
   this.pwidth=40; // width in pixels for drawing in context
 }
 
@@ -27,6 +48,8 @@ Cell.prototype.draw = function(ox,oy) {
   drawline(dl.x,dl.y,ul.x,ul.y, this.ridge.left);
 }
 
+
+// Global function to draw lines easily
 function drawline(sx,sy,dx,dy,color) {
   contxt.beginPath();
   if (color==1) { contxt.strokeStyle="#FF0000"} else {contxt.strokeStyle="#AAAAAA"}
@@ -35,26 +58,26 @@ function drawline(sx,sy,dx,dy,color) {
   contxt.stroke();
 }
  
-// Object Ridge creation
-function Ridge() {
- this.up = 0; 
- this.right =0;
- this.down=0;
- this.left=0;
-}
 
-// Function to draw the board on the screen
-// NOT USED
-var drawboard = function() {
-  contxt.strokestyle="#0000FF";
-  contxt.rect(100,100,200,25);
-  contxt.rect(300,300,200,25);
-  contxt.stroke();
+
+
+// Global variables ,and global common ridge arrays creation
+var sizer = 4; var sizec = 4;
+var cell =[];
+var ridgeh =[];
+var ridgev =[];
+for (var r=1;r<=sizer+1;r++)
+{
+  ridgeh[r]=[];
+  ridgev[r]=[];
+  for (var c=1;c<=sizec+1;c++)
+  {
+    ridgeh[r][c]= 0;
+    ridgev[r][c]= 0;
+  }
 }  
 
-
-var cell =[];
-var sizer = 4; var sizec = 4;
+// Main function, launched at html page loading
 
 var main = function() {
   var logoloopit;
@@ -73,6 +96,7 @@ var main = function() {
      for (var c=1;c<=sizec;c++)
      {
        cell[r][c]= new Cell(r,c);
+       cell[r][c].ridge = new Ridge(r,c);
      }
    }  
  
@@ -84,9 +108,9 @@ var main = function() {
   cell[2][1].ridge.left=1;
    
   // Additionnal identical values
-  cell[1][2].ridge.left=1;
-  cell[2][2].ridge.left=1;
-  cell[3][1].ridge.up=1;
+ // cell[1][2].ridge.left=1;
+ // cell[2][2].ridge.left=1;
+ // cell[3][1].ridge.up=1;
  
   // Now draw the Board
   for (var r=1;r<=sizer;r++)
@@ -99,7 +123,12 @@ var main = function() {
   
   console.log("start init");
   
-  console.log(cell[3][2].ridge.down);
+  console.log(cell[2][1].ridge.down);
+  console.log(cell[3][1].ridge.up);
+  cell[2][1].ridge.down = 0;
+  console.log(cell[2][1].ridge.down);
+  console.log(cell[3][1].ridge.up);
+  
   
   console.log("end init");
 
