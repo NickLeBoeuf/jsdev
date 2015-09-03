97ec3b101e292eb3be65fdcc9e2afe5e3a09f210
Nick <nick.leboeuf@gmail.com>
2015-09-03 22:35:39 +0200

    Modified script to display the wheel turning and progressively stop it.

--- roulette/js/main.js
+++ roulette/js/main.js
@@ -9,19 +9,40 @@ var main = function() {
   canvasLoopIt2DContext = canvasRoulette.getContext('2d');
   // use a shorter name for easyness
   contxt=canvasLoopIt2DContext;
-
+ 
   imgroulette = new Image();
   imgroulette.src = "./img/roulette.jpg";
   
   contxt.scale(0.5,0.5);
   contxt.translate(500,500);
   //contxt.drawImage(imgroulette,-504,-504);
+  angle = 0;
+  angle_increment = 0.1;
+  
 
 }
 
 var rotate_roulette = function() {
+ 
+  contxt.save();
   contxt.rotate(angle);
   contxt.drawImage(imgroulette,-504,-500);
+  angle = angle + angle_increment  ;
+  if (angle_increment > 0) 
+    angle_increment = angle_increment - 0.0003;
+  else 
+    angle_increment = 0;
+
+  
+  contxt.restore();
+  
+  contxt.clearRect(-480,-500,300,80);
+  contxt.fillText(angle.toString(), -480, -480);
+  contxt.fillText(angle_increment.toString(), -480, -450);
+  contxt.font="20px Verdana";
+  contxt.fillStyle = "black";
+  contxt.stroke();
+  
   
   requestAnimId = window.requestAnimationFrame(rotate_roulette);
    
