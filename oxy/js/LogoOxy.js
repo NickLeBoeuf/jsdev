6177c688aff3dec2d90c1b58a0a60fe0e07e05f5
nickleboeuf <nick.leboeuf@gmail.com>
2015-09-17 23:42:43 +0200

    Detection of Multiple Keyboard keys for LogoOxy
    Simple change of the else if into basic if...

--- oxy/js/LogoOxy.js
+++ oxy/js/LogoOxy.js
@@ -16,23 +16,26 @@ LogoOxy.prototype.reset = function () {
 };
 
 LogoOxy.prototype.handleInput = function (delta) {
+
+
+
     if (Keyboard._keyStates[Keys.Q].down)
         this.rotation += 0.01;
-    else if (Keyboard._keyStates[Keys.W].down)
+    if (Keyboard._keyStates[Keys.W].down)
         this.rotation -= 0.01;        
-    else if (Keyboard._keyStates[Keys.A].down)
+    if (Keyboard._keyStates[Keys.A].down)
         this.scale -= 0.01;        
-    else if (Keyboard._keyStates[Keys.S].down)
+    if (Keyboard._keyStates[Keys.S].down)
         this.scale += 0.01;        
-    else if (Keyboard._keyStates[Keys.left].down)
+    if (Keyboard._keyStates[Keys.left].down)
         this.position.x -= 1;
-    else if (Keyboard._keyStates[Keys.right].down)
+    if (Keyboard._keyStates[Keys.right].down)
         this.position.x += 1;
-    else if (Keyboard._keyStates[Keys.up].down)
+    if (Keyboard._keyStates[Keys.up].down)
         this.position.y -= 1
-    else if (Keyboard._keyStates[Keys.down].down)
+    if (Keyboard._keyStates[Keys.down].down)
         this.position.y += 1;      
-    else if (Keyboard._keyStates[Keys.T].down)
+    if (Keyboard._keyStates[Keys.T].down)
         this.visible = not(this.visible);        
 };
 
